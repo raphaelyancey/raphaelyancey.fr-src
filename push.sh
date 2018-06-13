940ae7f25c3602757e856e16e503b6ce72602286
Raphaël Yancey <raphael@badfile.net>
2018-06-13 20:16:06 +0200

    Improved push script

--- push.sh
+++ push.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
-git checkout gh-pages
-jekyll build
-git add -A
-git commit -am "Updated built files"
-git push origin gh-pages
-git checkout master
+git status
+
+build() {
+  git commit -am
+  jekyll build
+  cd dist
+  git add -A
+  git commit -am "Update"
+  git push origin master
+  cd ..
+}
+
+echo ""
+echo -n "=> Continue? (y/n)"
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    build
+else
+    exit 0
+fi
